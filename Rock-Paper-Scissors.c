3f20c35319a1e111146b8a08ccadda9a6c28384c
Censera <198231632+Censera@users.noreply.github.com>
2025-03-08 00:43:26 +0000

    Add files via upload

---
+++ Rock-Paper-Scissors.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int toNumber(char c){
+    switch (c)
+    {
+    case 'R':
+    case 'r':
+        return 1;
+        break;
+    case 'P':
+    case 'p':
+        return 2;
+        break;
+    case 'S':
+    case 's':
+        return 3;
+        break;
+    default:
+        return -1;
+        break;
+    }
+}
+
+int main () {
+    srand(time(NULL));
+    int Number = 3;
+    int randp = rand() % Number;
+    char player;
+
+    printf("[R]ock, [P]aper, [S]cissors :\n"); 
+    scanf("%c", &player);
+
+    player = toNumber(player);
+
+    if (player == -1) {
+        printf("Invalid input\n");
+        return 1;
+    }
+
+    if (player == randp) {
+        printf("Draw!");
+
+    } else if ( ( player == 1 && randp == 3 ) ||
+                ( player == 3 && randp == 2 ) ||
+                ( player == 2 && randp == 1 )
+              ) {
+        printf("You won!");
+
+    } else {
+            printf("You lose!");
+    }
+
+    return 0;
+}
