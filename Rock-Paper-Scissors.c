6776b361c7f9ba00c65db2d95cdd90dc182bf4de
Censera <198231632+Censera@users.noreply.github.com>
2025-03-09 00:42:56 +0000

    Update Rock-Paper-Scissors.c

--- Rock-Paper-Scissors.c
+++ Rock-Paper-Scissors.c
@@ -17,40 +17,51 @@ int toNumber(char ch){
     case 's':
         return 3;
         break;
+    case 'q':
+        return 4;
+        break;
     default:
         return -1;
         break;
     }
 }
+typedef struct {
+    int wins, loses, ties;
+} data;
 
 int main () {
     srand(time(NULL));
     int Number = 3;
-    int computer = (rand() % Number) + 1;
     char player;
+    data gameState = {0, 0, 0};
 
-    printf("[R]ock, [P]aper, [S]cissors :\n"); 
-    scanf("%c", &player);
+    while (player != 4) {
+        int computer = (rand() % Number) + 1;
+        printf("[R]ock, [P]aper, [S]cissors [q]uit: "); 
+        scanf(" %c", &player);
 
-    player = toNumber(player);
+        player = toNumber(player);
 
-    if (player == -1) {
-        printf("Invalid input\n");
-        return 1;
+        if (player == -1) {
+            printf("Invalid input\n");
+        }
+
+        if (player == computer) {
+            printf("Tie!\n");
+    
+            ++gameState.ties;
+      } else if ( ( player == 1 && computer == 3 ) ||
+                   ( player == 3 && computer == 2 ) ||
+                   ( player == 2 && computer == 1 )
+                ) {
+            printf("You Won!\n");
+
+            ++gameState.wins;
+       } else {
+            printf("You Lose!\n");
+           ++gameState.loses;
+        }
+        printf("Wins: %d; Loses: %d; Ties: %d\n",gameState.wins, gameState.loses, gameState.ties);
     }
-
-    if (player == computer) {
-        printf("Tie!");
-
-    } else if ( ( player == 1 && computer == 3 ) ||
-                ( player == 3 && computer == 2 ) ||
-                ( player == 2 && computer == 1 )
-              ) {
-        printf("You Won!");
-
-    } else {
-            printf("You Lose!");
-    }
-
     return 0;
 }
