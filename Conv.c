41d48378a7d2051d600bc62133a4d60aa5698ec8
Censera <198231632+Censera@users.noreply.github.com>
2025-11-10 23:03:40 +0100

    da

--- Conv.c
+++ Conv.c
@@ -1,29 +1,47 @@
 #include <stdio.h>
 
-int main(){
-char choice;
-double number, result = 0;
-printf("[l] is for Kilometers to Miles, [t] is for °C to °F [q] for exit:\n");
-scanf("%c", &choice);
-switch(choice){
-    case 'l':
-      printf("Kilometers to miles:\n \t- ");
-      scanf("%lf",&number);
-      result = number/1.609344;
-      printf("= %.2lf Miles.",result);
-      break;
-    case 't':
-      printf("°C to °F: \n \t- ");
-      scanf("%lf",&number);
-      result = (number*(9/5))+32;
-      printf("= %.2lf °F.",result);
-      break;
-    case 'q':
-      printf("quitting...\n");
-      break;
-    default:
-      printf("Invalid choice!");
-      break;
-  }
-  return 0;
+int main ()
+{
+    char choice;
+    double number;
+    double result = 0;
+    
+    printf("[l] is for Kilometers to Miles, [t] is for °C to °F [q] for exit:\n");
+    
+    scanf("%c", &choice);
+
+    while (1)
+    {
+      if (choice == 'q')
+      {
+          printf("Quitting...\n");
+          return 0;
+      }
+    }
+
+    switch (choice)
+    {
+      case 'l':
+          printf("Kilometers to miles:\n");
+          scanf("%lf", &number);
+
+          result = number / 1.609344;
+        
+          printf("= %.2lf Miles.", result);
+          break;
+      
+      case 't':
+          printf("°C to °F:\n");
+          scanf("%lf", &number);
+        
+          result = (number * (9.0 / 5)) + 32;
+        
+          printf("= %.2lf °F.", result);
+          break;
+
+      default:
+          printf("Invalid choice!\n");
+          break;
+    }
+    return 0;
 }
