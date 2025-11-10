c2c19d7875dbaec323e5005c303370711e217cb4
Censera <198231632+Censera@users.noreply.github.com>
2025-11-10 22:28:43 +0100

    hmm

--- autoShutdown.c
+++ autoShutdown.c
@@ -2,34 +2,39 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int shut_down () {
-        char* cmd[] = {"/sbin/shutdown", "-h", "now", NULL};
-        execvp(cmd[0], cmd);
-        exit(1);
+void    shut_down (void)
+{
+        system("shutdown -h now");
 }
-int message () {
-        system("zenity --info --text='Will shutdown after 30 seconds after clicking OK'");
-        sleep(30);
+int     message ()
+{
+        // Install Zenity
+        if (system("zenity --info --text='It's time to shudown the system'") != 0)
+                return -1;
         return 0;
 }
         
-int check_time (int from, int to, int time_hour) {
-        if (time_hour >= from && time_hour <= to) {
+void     check_time (int from, int to, int time_hour)
+{
+        if (time_hour >= from && time_hour <= to)
+        {
                 message();
                 shut_down();
         }
-        return 0;
 }
 
-int main () {
-        while (1) {
+int main ()
+{
+        int cur_hour = 0;
+
+        while (1)
+        {
                 time_t now = time(NULL);
                 struct tm *myLocalTime = localtime(&now);
-                int cur_hour = myLocalTime -> tm_hour;
+                cur_hour = myLocalTime -> tm_hour;
 
                 check_time(0, 3, cur_hour);
                 sleep(60);
         }
-
         return 0;
 }
