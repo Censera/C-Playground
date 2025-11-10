8836a6dd497aa67e0766145a911bd8c3b4f755c5
Censera <198231632+Censera@users.noreply.github.com>
2025-11-10 23:14:12 +0100

    x

--- alphabet.c
+++ alphabet.c
@@ -4,17 +4,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main()
+int main(void)
 {
     char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     char guess;
     int alphabet_len = strlen(alphabet);
     char winning_letter = alphabet[rand() % alphabet_len];
     int attempts = alphabet_len; //26
+
     srand(time(NULL));
 
     printf("- Guess a letter : ", winning_letter);
     scanf("%c", &guess);
+    
     guess = toupper(guess);
 
     while (1)
@@ -23,7 +25,7 @@ int main()
             scanf("\n%c", &guess);
             guess = toupper(guess);
             
-            if ( guess == winning_letter )
+            if (guess == winning_letter)
             {
                 printf("You won with %d attempts, and the winning letter is %c", attempts, winning_letter);
                 break;
@@ -31,7 +33,7 @@ int main()
 
             attempts--;
             
-            if ( 0 == attempts )
+            if (attempts == 0)
             {
                 printf("You lost, it's %c", winning_letter);
                 break;
