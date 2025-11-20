34fd9979a557059af51ab3c77ac16ad19877047a
Censera <Censera@proton.me>
2025-11-20 16:50:42 +0100

    overflow

--- Overflow/overflow.c
+++ Overflow/overflow.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 
-int main(void){
-  unsigned char binary = 0b100000000;
+int main(void)
+{
+  unsigned char  c; // 1 byte
+  short   a; // 2 bytes
 
-  printf("%d", binary);
+  c = 0b11111111; // 255
+  
+  a = 0b11111111; // 255
+
+  printf("1 byte:\nc = %d\n", c);
+  
+  // add 1 to c
+  c++;
+  printf("c + 1 = %d\n", c);
+
+  printf("\n2 bytes:\na = %d\n", a);
+  
+  // add 1 to a  
+  a++;
+  printf("a + 1 = %d\n", a);
 
   return 0;
 }
